ec3626368461b3d576b15452d9df0838755af400
IljaFatkulin <ilja.fatkulin@gmail.com>
2024-10-03 23:17:57 +0300

    removed dev server from capacitor config

--- packages/demo/capacitor.config.ts
+++ packages/demo/capacitor.config.ts
@@ -4,10 +4,10 @@ const config: CapacitorConfig = {
   appId: "scandiweb.print",
   appName: "demo",
   webDir: "dist",
-  server: {
-    url: "http://192.168.8.56:5173",
-    cleartext: true,
-  },
+  // server: {
+  //   url: "http://192.168.8.56:5173",
+  //   cleartext: true,
+  // },
 };
 
 export default config;
